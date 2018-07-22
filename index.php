49d8f9919f6a840df66d807093a7c02ca3ca099b
kalaiselvan <kalai6095@gmail.com>
2018-07-23 00:35:17 +0530

    token changed with new

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 if(isset($_REQUEST['hub_challenge'])){
 	$challenge=$_REQUEST['hub_challenge'];
-	$token=$_REQUEST['hub_verfiy_token'];	
+  $token=$_REQUEST['hub_verfiy_token'];	
+  echo $challenge;
 }
 if($token=="kalaiCustomToken123"){
 	echo $challenge;
 }
+
 ?>
